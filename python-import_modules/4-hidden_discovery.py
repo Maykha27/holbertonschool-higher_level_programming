53716a99b84ffd934167ad78009233cf15492642
Maykha27 <5144@holbertonschool.com>
2022-09-11 12:06:47 +0100

    4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-if __name__=="__main__":
-    from variable_load_5 import a
-    print("{:d}".format(a))
+if __name__ == "__main__":
+    import hidden_4 hidden_4
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2]!="__":
+            print(name)
