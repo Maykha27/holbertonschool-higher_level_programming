54e5745953fc58b3ee3bb87c4a14cad674ac112a
Maykha27 <5144@holbertonschool.com>
2022-09-15 13:45:59 +0100

    10-best_score.py

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is Nome:
+        returnNome
+
+    best_score = max(a_dictionary.value(), default=None)
+    for a, b in a_dictionary.items():
+        if b == best_score:
+            return a
