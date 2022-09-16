7ae1189bc2332a3ef04acaf21d7f38a6ddac519e
Maykha27 <5144@holbertonschool.com>
2022-09-16 15:08:52 +0100

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
+#!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     idx = 0
-    try:
-     for i in my_list:
-            if idx < x:
-                print('{}'.format(my_list[idx]), end='')
-                idx += 1
-    except TypeError:
-        pass
-    finally:
-        return idx
+    for i in range(x):
+        try:
+            print('{}'.format(my_list[i]), end='')
+
+        except IndexError:
+            pass
+        else:
+            idx += 1
+    print()
+    return idx
