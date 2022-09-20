38acf6abc4c4e0139756aec38c863bb01706dbd2
Maykha27 <5144@holbertonschool.com>
2022-09-20 11:09:53 +0100

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Square that defines a square"""
 class Square:
+    """class defined for square generation
+    """
       def  __init__ ( self, size ):
         self . __size = size
