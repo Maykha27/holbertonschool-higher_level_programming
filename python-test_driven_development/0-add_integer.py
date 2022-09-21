63b6b754fb4f2dc2eb9f58f2130fa030f088efa7
Maykha27 <5144@holbertonschool.com>
2022-09-21 13:01:11 +0100

    0-add_integer.py

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+Module add_integer define function
+to do add operation and handle Type
+Error
+"""
+
+def add_integer(a, b=98):
+    """ add to integer or float
+    Args:
+    a (int): first number
+    b (int): second number (optional)
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+
