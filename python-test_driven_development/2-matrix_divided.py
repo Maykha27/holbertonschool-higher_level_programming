df19690790996edc84c5b31fdf65619b223632fe
Maykha27 <5144@holbertonschool.com>
2022-09-22 15:48:29 +0100

    2-matrix_divided.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -18,5 +18,5 @@ def matrix_divided(matrix, div):
     if not all(len(lists) == len(matrix[0]) for lists in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if div == 0:
-        raise ZeriDivisionError("division by zero")
+        raise ZeroDivisionError("division by zero")
     return[[round(item / div, 2) for item in lists] for lists in matrix]
