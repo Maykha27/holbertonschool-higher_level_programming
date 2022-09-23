e4ed9e01fc562cccc26190f253bcdb8ccd6eec11
Maykha27 <5144@holbertonschool.com>
2022-09-23 20:49:30 +0100

    4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+Function to print a square
+"""
+
 def print_square(size):
+     """
+    Prints a square using '#'
+    """ 
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
