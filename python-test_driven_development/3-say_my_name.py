ce6fd293caf2edd7930600a56834b8da5a9a693b
Maykha27 <5144@holbertonschool.com>
2022-09-23 20:34:20 +0100

    3-say_my_name.py

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
-def say_my_name(first_name, last_name=""):
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
-    print("My name is {:s} {:s}".format(first_name, last_name))
+"""
+Function to print a phrase
+"""
+
+
+def  say_my_name ( first_name ,  last_name = "" ):
+    """
+    Prints a phrase with:
+    first_name: string to be printed
+    last_name: second string to be printed
+    Return: A formatted output with the strings.
+    """
+    if  type ( first_name )  not  in  [ str ]:
+        raise  TypeError ( "first_name must be a string" )
+    if  type ( last_name )  not  in  [ str ]:
+        raise  TypeError ( "last_name must be a string" )
+    print ( "My name is {} {}" . format ( first_name ,  last_name )) 
--- python-test_driven_development/4-test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-""" Doc """
-
-print_square = __import__('4-print_square').print_square
-
-print_square(0)
