b9010c9ce846acc2f4abf384631c42372a84cac0
Maykha27 <5144@holbertonschool.com>
2022-09-28 15:23:01 +0100

    1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -7,7 +7,7 @@ Write a class MyList that inherits from list
 class MyList(list):
     """contains list
     """
-def print_sorted(self):
-    """print_sorted-method to sort a list"""
-    print(sorted(self[:]))
+    def print_sorted(self):
+        """print_sorted-method to sort a list"""
+        print(sorted(self[:]))
 
