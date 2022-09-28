5579a16d674f2778ec833ffe45af74a0c79341f4
Maykha27 <5144@holbertonschool.com>
2022-09-28 10:51:36 +0100

    6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Write a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry"""
+    def area(self):
+        """added the method 'area'"""
+        raise Exception("area() is not implemented")
