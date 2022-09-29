024875f41f127cf2623c40af783ebe5448176d2a
Maykha27 <5144@holbertonschool.com>
2022-09-29 01:02:31 +0100

    11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Class BaseGeometry based in the last task
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square data that inherits from Rectangle"""
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def area(self):
+        """the area() method must be implemented
+        """
+
+        return self.__size*self.__size
+
+    def __str__(self):
+        """
+            should return, the following rectangle description
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
