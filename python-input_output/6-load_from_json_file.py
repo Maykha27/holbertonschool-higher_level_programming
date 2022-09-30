ee171d1e922e8d3d1d005d5046f433f087f650d4
Maykha27 <5144@holbertonschool.com>
2022-09-30 17:39:36 +0100

    6-load_from_json_file.py

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -3,7 +3,9 @@
 
 
 import json
+
+
 def load_from_json_file(filename):
     """fonction"""
-    with open (filename, "r", encoding="UTF-8") as file:
+    with open(filename, "r", encoding="UTF-8") as file:
         return json.load(file)
