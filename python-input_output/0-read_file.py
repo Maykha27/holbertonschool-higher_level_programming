64acb197ef81506810238ed459734ea9e36c425d
Maykha27 <5144@holbertonschool.com>
2022-09-30 11:30:52 +0100

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""a function that reads a text file"""
+
+
 def read_file(filename=""):
     """fonction that read _file """
     with open(filename, encoding='utf-8') as a_file:
