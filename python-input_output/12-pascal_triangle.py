801d78efe25e4be831f59b8d7e4667c4a8c1cb7c
Maykha27 <5144@holbertonschool.com>
2022-09-30 22:57:26 +0100

    12-pascal_triangle.py

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""a function  that returns a list of lists of integers
+ representing the Pascal’s triangle of n:"""
+
+ 
 def pascal_triangle(n):
     """ Pascal Triangle 1h"""
     li = []
