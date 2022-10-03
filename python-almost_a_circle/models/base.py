18334406ea8987e724c542105fcebd03050b6472
Maykha27 <5144@holbertonschool.com>
2022-10-04 00:39:00 +0100

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -9,10 +9,120 @@ class Base:
 
     __nb_objects = 0
 
-    def __init__(self, id=None):
-        '''Constructor.'''
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+    def  __init__ ( self ,  id = None ):
+        "'Constructeur."'
+        si  l'id  n'est  pas  Aucun :
+            auto . id  =  id
+        autre chose :
+            De la Base . __nb_objets  +=  1
+            auto . id  =  Base . __nb_objets
+
+    @ staticmethod
+    def  to_json_string ( list_dictionaries ):
+        "'Jsonifies un dictionnaire, donc il est tout à fait juste et plus."'
+        si  list_dictionaries  est  Aucune  ou  pas  list_dictionaries :
+            retour  "[]"
+        autre chose :
+            retour  dumps ( list_dictionaries )
+
+    @ staticmethod
+    def  from_json_string ( json_string ):
+        "'Unjsonifies un dictionnaire."'
+        si  json_string  est  Aucune  ou  pas  json_string :
+            de retour  []
+        de retour de  charges ( json_string )
+
+    @ classmethod
+    def  save_to_file ( cls ,  list_objs ):
+        ""Permet de gagner du jsonified objet dans un fichier."'
+        si  list_objs  est  pas  Aucun :
+            list_objs  =  [ o . to_dictionary ()  pour  o  dans  list_objs ]
+        avec  open ( "{}.json" . format ( cls . __nom__ ),  "w" ,  encoding = "utf-8" )  comme  f :
+            f . écrire ( cls . to_json_string ( list_objs ))
+
+    @ classmethod
+    def  load_from_file ( cls ):
+        ""Charge de chaîne à partir d'un fichier et unjsonifies."'
+        à partir d'  os  d'importation  chemin
+        fichier  =  "{}.json" . format ( cls . __nom__ )
+        si  pas  de chemin d'accès . isfile ( fichier ):
+            de retour  []
+        avec  open ( fichier ,  "r" ,  encoding = "utf-8" )  comme  f :
+            de retour  [ cls . créer ( ** d )  pour  d  dans  cls . from_json_string ( f . lire ())]
+
+    @ classmethod
+    def  créer ( cls ,  ** dictionnaire ):
+        "'Les charges d'instance à partir du dictionnaire."'
+        à partir de  modèles . rectangle  d'importation  Rectangle
+        à partir de  modèles . carré  d'importation  Carré
+        si  cls  est  Rectangle :
+            nouveau  =  Rectangle ( 1 ,  1 )
+        elif  cls  est  Carré :
+            nouveau  =  Carré ( 1 )
+        autre chose :
+            nouveau  =  Aucun
+        de nouvelles . mise à jour ( ** dictionnaire )
+        de retour de  nouveau
+
+    @ classmethod
+    def  save_to_file_csv ( cls ,  list_objs ):
+        ""Permet de gagner de l'objet au format de fichier csv."'
+        à partir de  modèles . rectangle  d'importation  Rectangle
+        à partir de  modèles . carré  d'importation  Carré
+        si  list_objs  est  pas  Aucun :
+            si  cls  est  Rectangle :
+                list_objs  =  [[ o . id ,  o . largeur ,  o . hauteur ,  o . x ,  o . y ]
+                             pour  ou  dans  list_objs ]
+            autre chose :
+                list_objs  =  [[ ou . id ,  ou . taille ,  ou . x ,  ou . et ]
+                             pour  ou  dans  list_objs ]
+        avec  open ( '{}.csv' . format ( cls . __nom__ ),  'w' ,  newline = " ,
+                  encoding = 'utf-8' )  comme  f :
+            auteur  =  csv . auteur ( f )
+            de l'écrivain . writerows ( list_objs )
+
+    @ classmethod
+    def  load_from_file_csv ( cls ):
+        "'Les charges de l'objet au format de fichier csv."'
+        à partir de  modèles . rectangle  d'importation  Rectangle
+        à partir de  modèles . carré  d'importation  Carré
+        ret  =  []
+        avec  open ( '{}.csv' . format ( cls . __nom__ ),  'r' ,  newline = " ,
+                  encoding = 'utf-8' )  comme  f :
+            lecteur  =  csv . lecteur ( f )
+            pour  ligne  dans le  lecteur :
+                ligne  =  [ int ( r )  pour  r  en  ligne ]
+                si  cls  est  Rectangle :
+                    d  =  { "id" :  row [ 0 ],  "width" :  la ligne [ 1 ],  "height" :  la ligne [ 2 ],
+                         "x" :  la ligne [ 3 ],  "y" :  la ligne [ 4 ]}
+                autre chose :
+                    d  =  { "id" :  row [ 0 ],  "taille" :  la ligne [ 1 ],
+                         "x" :  la ligne [ 2 ],  "y" :  row [ 3 ]}
+                ret . append ( cls . créer ( ** d ))
+        retour  ret
+
+    @ staticmethod
+    def  tirage ( list_rectangles ,  list_squares ):
+        l'importation  de la tortue
+        l'importation  de temps
+        de  random  import  randrange
+        la tortue . Écran (). colormode ( 255 )
+        pour  je  dans  list_rectangles  +  list_squares :
+            t  =  tortue . Tortue ()
+            t . couleur (( randrange ( 255 ),  randrange ( 255 ),  randrange ( 255 )))
+            t . pensize ( 1 )
+            t . penup ()
+            t . pendown ()
+            t . setpos (( j' . x  +  t . pos ()[ 0 ],  j' . y  -  t . pos ()[ 1 ]))
+            t . pensize ( 10 )
+            t . avant ( j' . la largeur )
+            t . gauche ( 90 )
+            t . avant ( j' . hauteur )
+            t . gauche ( 90 )
+            t . avant ( j' . la largeur )
+            t . gauche ( 90 )
+            t . avant ( j' . hauteur )
+            t . gauche ( 90 )
+            t . end_fill ()
+
+        temps . sommeil ( 5 ) 
