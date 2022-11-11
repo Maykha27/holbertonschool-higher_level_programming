91c8e6e30c9b74978941cfa7649ab0f7cfe5d727
Maykha27 <5144@holbertonschool.com>
2022-11-11 11:34:38 +0100

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-
-#lists all states
+# Lists all states with a name starting with N from the database hbtn_0e_0_usa.
+# Usage: ./1-filter_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
 
 import MySQLdb
+import sys
 
 
 if__name__ == "__main__":
