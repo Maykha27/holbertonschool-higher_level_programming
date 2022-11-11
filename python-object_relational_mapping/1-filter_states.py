ddf9cdf3ce3d81d80c9c0d0c72964e1f6abab650
Maykha27 <5144@holbertonschool.com>
2022-11-11 11:26:51 +0100

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,14 +6,14 @@ from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
-from sys import argv
+
 
 if__name__ == "__main__":
     db = MySQLdb.connect(host='localhost',
-                            port=3306,
-                            user=argv[1],
-                            passwd=argv[2],
-                            db=argv[3])
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        port=3306)
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     for state in cursor.fetchall():
