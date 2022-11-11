7e6b4d42906df5db4bf625587f8e733a815da58c
Maykha27 <5144@holbertonschool.com>
2022-11-11 11:42:55 +0100

    2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
+"""
+Displays all values in the states table of the database hbtn_0e_0_usa
+"""
 import sys
 import MySQLdb
 
