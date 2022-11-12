940fbc07ba91a46fe280ce1884af44f1fc97140c
Maykha27 <5144@holbertonschool.com>
2022-11-12 14:13:33 +0100

    1-filter_states.py

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 """
-Lists all states with a name
+lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
 """
+
 import MySQLdb
-import sys
+from sys import argv
 
-
-if__name__ == "__main__":
-    db = MySQLdb.connect(host='localhost',
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3],
-                        port=3306)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-    for state in cursor.fetchall():
-        if state[1][0] == "N":
+    cursor.execute('SELECT * FROM states ORDER BY states.id ASC')
+    rows = cursor.fetchall()
+    for state in rows:
+        if state[1][0] == 'N':
             print(state)
