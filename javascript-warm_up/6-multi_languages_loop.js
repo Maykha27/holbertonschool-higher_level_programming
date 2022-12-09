930225ab72317af8e1fa9224eddac1a1a9896880
Maykha27 <5144@holbertonschool.com>
2022-12-09 17:33:33 +0100

    6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // script that prints 3 lines: (like 1-multi_languages.js)
 
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < 3; i++) {
   console.log(langs[i]);
