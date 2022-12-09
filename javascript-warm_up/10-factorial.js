00d8bddee05f18e7381630df7d52aac0bffed8f3
Maykha27 <5144@holbertonschool.com>
2022-12-09 20:38:47 +0100

    10-factorial.js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -2,11 +2,11 @@
 // script that computes and prints a factorial.
 
 function factorial (n) {
-    if ((isNaN(n)) || (n === 1)) {
-       return 1;
-     } else {
+  if ((isNaN(n)) || (n === 1)) {
+    return 1;
+  } else {
     return n * factorial(n - 1);
-     }
+  }
 }
 
-  console.log(factorial(parseInt(process.argv[2])));
+console.log(factorial(parseInt(process.argv[2])));
