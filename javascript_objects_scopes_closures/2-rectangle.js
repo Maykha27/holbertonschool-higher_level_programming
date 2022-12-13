5552620df4d1464887cf5a0799b632e46e7a9b10
Maykha27 <5144@holbertonschool.com>
2022-12-13 14:46:18 +0100

    2-rectangle.js

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// define a class and initialize with w, h
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+     return this;
+   } else {
+     this.width= w;
+     this.height= h;
+   }
+  }
+}
+module.exports = Rectangle;
