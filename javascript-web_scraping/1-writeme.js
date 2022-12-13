389bd5fee97d6f9dae14b7ff4958aa816948130e
Maykha27 <5144@holbertonschool.com>
2022-12-13 13:06:02 +0100

    1-writeme.js

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Write a script that writes a string to a file.
+
+const filesystem = require('fs');
+
+filesystem.writeFile(process.argv[2], process.argv[3], function (err) {
+  if (err) {
+    console.log(err);
+  }
+});
