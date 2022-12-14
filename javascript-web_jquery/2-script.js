c0507a2f3f7b3a02debba105cf1b052810a93392
Maykha27 <5144@holbertonschool.com>
2022-12-14 17:53:57 +0100

    3-script.js

--- javascript-web_jquery/2-script.js
+++ javascript-web_jquery/2-script.js
@@ -1,4 +1,4 @@
 // Write a JavaScript script that updates the text color
 $('#red_header').click(function () {
-	$('header').css('color', '#FF0000');
-  });
+  $('header').css('color', '#FF0000');
+});
