9f30fd95b668020e0c85d4e18ec04f2ae8b60ef4
Maykha27 <5144@holbertonschool.com>
2022-12-22 23:18:38 +0100

    3-rectangle.js

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,23 +1,24 @@
 #!/usr/bin/node
-// 
+// Write a class Rectangle that defines a rectangle
+// Create an instance method called print()
 
 class Rectangle {
-	constructor (w, h) {
-	  if (w > 0 && h > 0) {
-		this.width = w;
-		this.height = h;
-	  }
-	}
-
-	print () {
-	  let row = '';
-	  for (let i = 0; i < this.width; i++) {
-		row = row + 'X';
-	  }
-	  for (let i = 0; i < this.height; i++) {
-		console.log(row);
-	  }
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 
-  module.exports = Rectangle;
+  print () {
+    let row = '';
+    for (let i = 0; i < this.width; i++) {
+      row = row + 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Rectangle;
