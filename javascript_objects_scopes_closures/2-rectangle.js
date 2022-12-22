3acbbdf36f8a42c50e0989f022dcfd1785059952
Maykha27 <5144@holbertonschool.com>
2022-12-22 23:24:50 +0100

    2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -4,11 +4,11 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-     return this;
-   } else {
-     this.width= w;
-     this.height= h;
-   }
+      return this;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
   }
 }
 module.exports = Rectangle;
